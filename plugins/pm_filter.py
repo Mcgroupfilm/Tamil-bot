55a8aa3cf5962ce5fd006d53b163fb2fa9aac091
Githubstudent100 <134201294+Githubstudent100@users.noreply.github.com>
2023-07-23 17:15:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1880,7 +1880,7 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="𝐍𝐎 𝐌𝐎𝐑𝐄 𝐏𝐀𝐆𝐄𝐒 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄",callback_data="pages")]
         )
-    # imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
+    imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
     time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
     remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
@@ -1972,7 +1972,7 @@ async def auto_filter(client, msg, spoll=False):
     #             await fek.delete()
     #             await message.delete()
     # else:
-        fuk = await message.reply_photo(photo=photo, text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+        fuk = await message.reply_photo(photo=imdb.get('poster'), text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
     
         await m.delete()
         try:
