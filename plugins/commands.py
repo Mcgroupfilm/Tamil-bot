ee1c41133e9a6d1c0b3a782c1502bde99f048074
Githubstudent100 <134201294+Githubstudent100@users.noreply.github.com>
2023-07-24 19:01:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,3 +1,44 @@
+# Kanged From @TroJanZheX
+import asyncio
+import re
+import ast
+import math
+import random
+import pytz
+from datetime import datetime, timedelta, date, time
+lock = asyncio.Lock()
+
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from Script import script
+import pyrogram
+from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
+    make_inactive
+from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, SUPPORT_CHAT_ID, CUSTOM_FILE_CAPTION, MSG_ALRT, PICS, AUTH_GROUPS, P_TTI_SHOW_OFF, GRP_LNK, CHNL_LNK, NOR_IMG, LOG_CHANNEL, SPELL_IMG, MAX_B_TN, IMDB, \
+    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE, NO_RESULTS_MSG, TUTORIAL, REQST_CHANNEL, IS_TUTORIAL, LANGUAGES, SEASONS, SUPPORT_CHAT, PREMIUM_USER
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from pyrogram import Client, filters, enums
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_tutorial, send_all
+from database.users_chats_db import db
+from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
+from database.filters_mdb import (
+    del_all,
+    find_filter,
+    get_filters,
+)
+from database.gfilters_mdb import (
+    find_gfilter,
+    get_gfilters,
+    del_allg
+)
+
+
+
+
+
+
+
+
 import os
 import logging
 import random
@@ -412,7 +453,7 @@ async def start(client, message):
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
-            content = message.text            
+            search = message.text            
             TEMPLATE = script.IMDB_TEMPLATE_TXT
             cap = TEMPLATE.format(
             qurey = search,
@@ -463,7 +504,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
-    content = message.text            
+    search = message.text            
     TEMPLATE = script.IMDB_TEMPLATE_TXT
     cap = TEMPLATE.format(
     qurey = search,
