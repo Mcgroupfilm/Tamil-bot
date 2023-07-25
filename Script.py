1845ae921047d21cbfb4f58aeda07d278104df1e
Githubstudent100 <134201294+Githubstudent100@users.noreply.github.com>
2023-07-26 02:40:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1159,3 +1159,10 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
                        
 <b>⚙️ Fɪʟᴇ Sɪᴢᴇ</b> : <b>{}</b>
 """
+
+
+    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+
+    
