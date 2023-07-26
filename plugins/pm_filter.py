c49dcd7b7c069e6d88db24a3a48b23f682b44517
Githubstudent100 <134201294+Githubstudent100@users.noreply.github.com>
2023-07-26 11:40:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,14 +65,12 @@ RUN_STRINGS = (
     "🎭",    
 )
 
-def convert(msg, text):
-    audio = BytesIO()
-    g = msg.text
+def convert(text):
+    audio = BytesIO()    
     i = Translator().translate(text, dest="en")
     lang = i.src
     tts = gTTS(text, lang=lang)
-#    audio.name = lang + ".mp3"
-    audio.name = g + ".mp3"
+    audio.name = lang + "@nasrani_update.mp3"
     tts.write_to_fp(audio)
     return audio
 
